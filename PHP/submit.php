84f110e9c891f7fce924922344825e66f7d67155
Todor Kolev <todorkolev24@gmail.com>
2024-11-08 13:10:38 +0200

    Добавено поле в базата данни

--- PHP/submit.php
+++ PHP/submit.php
@@ -11,12 +11,13 @@ if ($conn->connect_error) {
 }
 
 $name = $_POST['name'];
+$beer_type = $_POST['beer_type'];
 $amount = $_POST['amount'];
 $phone_number = $_POST['phone_number'];
 $email = $_POST['email'];
 $home_address = $_POST['home_address'];
 
-$sql = "INSERT INTO order_user (name, amount, phone_number, email, home_address) VALUES ('$name', '$amount', '$phone_number', '$email', '$home_address')";
+$sql = "INSERT INTO order_user (name, beer_type, amount, phone_number, email, home_address) VALUES ('$name', '$beer_type', '$amount', '$phone_number', '$email', '$home_address')";
 if ($conn->query($sql) === TRUE) {
     echo "Your beer is coming :)";
 } else {
